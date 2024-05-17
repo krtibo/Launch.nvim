414490a3abbe6dc98d96c2f79df6cd8964cc48fe
Tibor Krizsák <t.krizsak@topdesk.com>
2024-05-17 15:40:26 +0200

    move new highlight setting to colorscheme

--- lua/user/highlights.lua
+++ lua/user/highlights.lua
@@ -1,7 +1,6 @@
 -- for some reason a plugin overrides the Pmenu colors to something else 
 -- this is a fix for that issue
 vim.api.nvim_set_hl(0, "Pmenu", { fg='#9399b3', bg='#1a1b26'})
-vim.api.nvim_set_hl(0, "WinBarNC", { fg='#9399b3', bg='#1a1b26'})
 
 local icons = require "user.icons"
 vim.diagnostic.config {
