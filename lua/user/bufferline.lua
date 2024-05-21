63b3c69c9c7f0e5681ebe5bc67a97176e45acfa1
Tibor Krizsák <t.krizsak@topdesk.com>
2024-05-21 11:23:33 +0200

    adjust bufferline indicator

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -9,10 +9,15 @@ function M.config()
 			numbers = function(opts)
 				return string.format('%s', opts.raise(opts.ordinal))
 			end,
+			themable = true,
 			show_buffer_icons = false,
 			separator_style= "thin",
 			always_show_bufferline = false,
 			auto_toggle_bufferline = true,
+			indicator = {
+				icon = '█ ',
+				style = 'icon',
+			},
 		},
 		highlights = require("flannel.groups.integrations.bufferline").get()
 	}
