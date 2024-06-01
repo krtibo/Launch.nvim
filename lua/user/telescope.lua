54470a1e1f66c9552147dc4956b259d01a9a2f35
Tibor Krizsák <t.krizsak@topdesk.com>
2024-06-01 18:23:36 +0200

    adjusting setup

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -18,6 +18,7 @@ function M.config()
     ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
     ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
     ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
+    ["<leader>fg"] = { "<cmd>Telescope git_status<cr>", "Git status" },
   }
 
   local icons = require "user.icons"
