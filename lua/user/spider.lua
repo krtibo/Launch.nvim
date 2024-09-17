0dea8ca7878a65bb2d0886fd93871316e020b588
Tibor Krizsak <tibork@topdesk.com>
2024-09-17 16:46:47 +0200

    add spider plugin

---
+++ lua/user/spider.lua
@@ -0,0 +1,21 @@
+return {
+	"chrisgrieser/nvim-spider",
+	lazy = true,
+	keys = {
+		{
+			"W",
+			"<cmd>lua require('spider').motion('w')<CR>",
+			mode = { "n", "o", "x" },
+		},
+		{
+			"E",
+			"<cmd>lua require('spider').motion('e')<CR>",
+			mode = { "n", "o", "x" },
+		},
+		{
+			"B",
+			"<cmd>lua require('spider').motion('b')<CR>",
+			mode = { "n", "o", "x" },
+		},
+	}
+}
